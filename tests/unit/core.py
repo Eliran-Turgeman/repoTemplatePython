45ecf2ff8b7d71631ca842a3d6cd262f1db4a460
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-05-11 23:01:16 +0300

    Create core.py

---
+++ tests/unit/core.py
@@ -0,0 +1,12 @@
+from YOUR_PACKAGE_NAME.core import get_message
+
+
+def test_get_message():
+  # Arrange
+  expected_message = "hello"
+  
+  # Act
+  message = get_message()
+  
+  # Assert
+  assert expected_message = message
