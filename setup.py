40507fbafe737ae30c8e6b455d728de7f28e6787
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-05-11 22:51:19 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+from importlib import util
+from os import path
+import os
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+spec = util.spec_from_file_location(
+    "YOUR_PACKAGE_NAME.version", os.path.join("YOUR_PACKAGE_NAME", "version.py")
+)
+mod = util.module_from_spec(spec)  # type: ignore[arg-type]
+spec.loader.exec_module(mod)  # type: ignore[union-attr]
+version = mod.version
+
+
+setup(
+    name='YOUR_PACKAGE_NAME',
+    license="Apache License 2.0",
+    version=version,
+    author='AUTHOR_NAME',
+    author_email='AUTHOR_EMAIL',
+    description='SHORT_DESCRIPTION',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/AUTHOR_NAME/YOUR_PACKAGE_NAME',
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        'requests',
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "coverage",
+            "mypy",
+            "ruff",
+        ]
+    },
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "YOUR_PACKAGE_NAME=YOUR_PACKAGE_NAME.main:main",
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
