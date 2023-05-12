b42ab2f9024cf9919c23215cfa84f033368b5022
GitHub Action <your.email@example.com>
2023-05-12 15:34:20 +0000

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 setup(
     name='YOUR_PACKAGE_NAME',
     license="MIT License",
-    version='0.0.0',
+    version='0.0.1',
     author='AUTHOR_NAME',
     author_email='AUTHOR_EMAIL@gmail.com',
     description='SHORT_DESCRIPTION',
